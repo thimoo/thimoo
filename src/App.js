f1370a6c267674b64aaacfea624012d377e8656b
Joel Gugger <joel@thimoo.ch>
2017-03-30 14:09:11 +0200

    basic project structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './header/Header.js';
+import Portfolio from './portfolio/Portfolio.js';
+import Contact from './contact/Contact.js';
+import Footer from './footer/Footer.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header></Header>
+        <Portfolio></Portfolio>
+        <Contact></Contact>
+        <Footer></Footer>
       </div>
     );
   }
---
+++ src/portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Portfolio.css';
+
+class Portfolio extends Component {
+  render() {
+    return (
+      <div className="Portfolio">
+        my Portfolio
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
